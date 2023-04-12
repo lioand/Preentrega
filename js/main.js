2e72e0622b81a722ea3f899d5b3e77eeb4dd3cf7
lio and <lio96and@gmail.com>
2023-04-12 02:27:16 -0300

    segunda entrega

--- js/main.js
+++ js/main.js
@@ -15,6 +15,39 @@ class Linea {
         this.pinicial = pinicial;
         this.pfinal = pfinal;
     }
+    getDist(){
+        let xf = this.pfinal.x;
+        let yf = this.pfinal.y;
+        let xi = this.pinicial.x;
+        let yi = this.pinicial.y;
+        return Math.sqrt(Math.pow(xf-xi , 2)+Math.pow(yf - yi , 2));;
+    }
+
+    getpendiente(inputPrompt){
+        let horiz = this.getDist();                     //obtengo la distancia horizontal
+        let vert = this.pfinal.z - this.pinicial.z;     //obtengo la distancia vertical 
+        let salida = vert / horiz ;
+        if (horiz == 0) {
+             salida = "Vertical:";
+            if (vert == 0) {
+                salida = "Los puntos son iguales";3
+            }
+            return salida;    
+        }else{
+            switch (inputPrompt) {
+                case 1: //en porcentaje
+                    salida = salida *100 + " %" 
+                    return salida;    
+                case 2: //en y/x
+                    return salida;    
+                case 3: //en grados
+                    salida = 180 * Math.atan(salida) / Math.PI + " °";
+                    return salida;    
+                default:
+                break;
+            }
+        }
+    }
 }
 
 
@@ -23,12 +56,22 @@ class Conjunto {
         this.array_puntos = [];
         this.array_lineas = [];
     }
+    inputdatoshard(){
+        //input datos de prueba hardcodeados
+            this.array_puntos.push(
+            new Punto (0,0,4.25, "punto1"),
+            new Punto (0,0,4.15, "punto2"),
+            new Punto (2,2,5.5, "punto3"),
+            new Punto (2,2,5.5, "punto4"),
+            new Punto (2,-2,0, "punto5"),
+            new Punto (-2,-2,0, "punto6"),
+        )
+        for (let i = 0; i < this.array_puntos.length-1; i++) {
+            let linea = new Linea (this.array_puntos[i + 1] , this.array_puntos[i]);
+            this.array_lineas.push(linea);
+        }  
+    }
     inputdatos() {
-        // this.array_puntos.push(
-        //     new Punto (0,0,4.25, "punto1"),
-        //     new Punto (3,6,4.15, "punto2"),
-        //     new Punto (3,7,4.3, "punto3"),
-        // )
         let inputPrompt;
         do {
             inputPrompt = prompt("Ingrese un punto (x,y,z,descripción) \no cancele para terminar",'0,0,0,tn');
@@ -37,7 +80,6 @@ class Conjunto {
                 inputPrompt = inputPrompt.split(",");
                 if (inputPrompt.length==3 || inputPrompt.length==4) {
                     if (!isNaN(inputPrompt[0]) && !isNaN(inputPrompt[1]) && !isNaN(inputPrompt[2])) {
-                        console.log("son numeros");
                         //si pasa las condiciones se crea el punto y lo agrego al array de puntos
                         this.array_puntos.push( 
                             new Punto ( parseFloat(inputPrompt[0]) , parseFloat(inputPrompt[1]) , parseFloat(inputPrompt[2]) , inputPrompt[3]));
@@ -51,32 +93,33 @@ class Conjunto {
             }
 
         } while (inputPrompt !== null)
+    }
 
+    lineaDistancia(){
+        return this.array_lineas.map((element) => element.getDist());
+    }
+    lineaPendiente(){
+        let inputPrompt;
+        do{
+            inputPrompt = parseInt(prompt("Ingrese el numero segun el formato de pendiente que desea: \n 1. Porcentaje   \n 2. y/x \n 3. Angulo"));
+        } while (inputPrompt != 1 && inputPrompt !=  2 && inputPrompt !=  3);
+        return this.array_lineas.map((element) => element.getpendiente(inputPrompt));
     }
 
     consola() {
         console.table(this.array_puntos);
         console.table(this.array_lineas);
+        console.table(this.lineaDistancia());
+        console.table(this.lineaPendiente());
+
     }
 }
 
 const conjpuntos = new Conjunto ();
 
+// conjpuntos.inputdatoshard(); //prueba con datos hardcodeados
 conjpuntos.inputdatos();
-// conjpuntos.consola()
 conjpuntos.consola();
-
-
-// console.log(isNaN(inputPrompt[0]));
-// console.log(isNaN(inputPrompt[1]));
-// console.log(isNaN(inputPrompt[2]));
-// console.log(isNaN(inputPrompt[3]));
-
-//console.table(array);
-
-
-//array[2].x;
-//console.log(array[2].x);
 
 
 
