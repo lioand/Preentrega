8033f7953fb3cb0035e0c6528cc8b8bde921bd04
lio and <lio96and@gmail.com>
2023-05-11 23:40:41 -0300

    Entrega 11-05-2023

---
+++ js/graph.js
@@ -0,0 +1,161 @@
+//-----------------------Dibujar Graficos en plotly------------------
+
+function getrandom(num , mul){ //crea un numero random
+   let value = [ ];
+   for(i=0;i<=num;i++)
+   {
+    let rand = Math.random() * mul;
+    value.push(rand);
+   }
+   return value;
+}
+
+const opciones = document.querySelectorAll('input[name="grafico"]');
+let seleccionAnterior = null;
+
+opciones.forEach(opcion => {
+  opcion.addEventListener('click', e => {
+    if (seleccionAnterior !== null) {
+      seleccionAnterior.checked = false;
+    }
+    seleccionAnterior = e.target;
+  });
+});
+
+//crep una funcion asincronica que genera un grafico 3d usando la libreria Plotly
+async function grafico3d(){
+    const mensajeCarga = document.getElementById("mensajeCarga");
+    const graficoContainer = document.getElementById("graph");
+  
+    mensajeCarga.style.display = "block"; // Mostramos el mensaje de carga
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    mensajeCarga.style.display = "none"; // Ocultamos el mensaje de carga
+
+    let data=[
+        {
+            mode: "lines+text",
+            type: 'scatter3d',
+            x: conjpuntos.getArrayX(),
+            y: conjpuntos.getArrayY(),
+            z: conjpuntos.getArrayZ(),
+            text: conjpuntos.getArrayD(),
+            scene: "scene1",
+            name: "Lower the view point",
+            color: "lightblue",
+            visible: true,
+            legendwidth: 20
+        }
+    ];
+    
+    
+    let layout = {
+        scene: {
+            xaxis:{title: 'X',
+            nticks: 10,
+            ticksuffix:'m'
+        },
+            yaxis:{title: 'Y',nticks: 10,
+            ticksuffix:'m'
+        },
+            zaxis:{title: 'Z',nticks: 10,
+            ticksuffix:'m'
+        }
+        },
+        autosize: false,
+        width: 1000,
+        height: 750,
+        plot_bgcolor: "#fff",
+    }	 
+    
+    Plotly.newPlot('graph', data, layout, {displaylogo: false}, {responsive: true});
+    graficoContainer.style.display = "block"; // Mostramos el contenedor del gráfico
+}
+
+//crep una funcion asincronica que genera un grafico visto desde arriba, en el plano x,y usando la libreria Plotly
+async function graficoplanta(){
+    const mensajeCarga = document.getElementById("mensajeCarga");
+    const graficoContainer = document.getElementById("graph");
+  
+    mensajeCarga.style.display = "block"; // Mostramos el mensaje de carga
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    mensajeCarga.style.display = "none"; // Ocultamos el mensaje de carga
+
+    let linea ={
+        x: conjpuntos.getArrayX(),
+        y: conjpuntos.getArrayY(),
+        text: conjpuntos.getArrayD(),
+        mode: 'lines+markers',
+        name: 'Scatter and Lines'
+    }
+
+    
+    let data=[linea];
+    
+    
+    let layout = {
+        
+        xaxis:{title: 'X',
+        nticks: 10,
+        ticksuffix:'m'
+        },
+        yaxis:{title: 'Y',nticks: 10,
+        ticksuffix:'m'
+        },
+        title: 'Planta',
+        width: 1300,
+        height: 750
+
+    };
+     
+    
+    Plotly.newPlot('graph', data, layout, {displaylogo: false}, {responsive: true});
+    graficoContainer.style.display = "block"; // Mostramos el contenedor del gráfico
+}
+
+//crep una funcion asincronica que genera un perfil longitudinal de los puntos usando la libreria Plotly
+async function graficoperfil(){
+    const mensajeCarga = document.getElementById("mensajeCarga");
+    const graficoContainer = document.getElementById("graph");
+    
+    mensajeCarga.style.display = "block"; // Mostramos el mensaje de carga
+
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    console.log(conjpuntos.getArrayXPerfil());
+    mensajeCarga.style.display = "none"; // Ocultamos el mensaje de carga
+
+    let linea ={
+        x: conjpuntos.getArrayXPerfil(),
+        y: conjpuntos.getArrayZ(),
+        text: conjpuntos.getArrayD(),
+        mode: 'lines+markers',
+        name: 'Scatter and Lines'
+    }
+
+    
+    let data=[linea];
+    
+    
+    let layout = {
+        
+        xaxis:{title: 'X',
+        nticks: 10,
+        ticksuffix:'m'
+        },
+        yaxis:{title: 'Y',nticks: 10,
+        ticksuffix:'m'
+        },
+        title: 'Planta',
+        width: 1300,
+        height: 750
+
+    };
+     
+    
+    Plotly.newPlot('graph', data, layout, {displaylogo: false}, {responsive: true});
+    graficoContainer.style.display = "block"; // Mostramos el contenedor del gráfico
+}
+
--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
-//construye puntos con x,y,z y descripcion(d)
+//----------Datos para mostrar u ocultar las distintas secciones del DOM
+//------------------------------------------------------------------------------------------------------------------
+// Obtengo los botones y las secciones de contenido
 
+
+const btnSeccion1 = document.getElementById("btn-seccion-1");
+const btnSeccion2 = document.getElementById("btn-seccion-2");
+const btnSeccion3 = document.getElementById("btn-seccion-3");
+const seccion1 = document.getElementById("seccion-1");
+const seccion2 = document.getElementById("seccion-2");
+const seccion3 = document.getElementById("seccion-3");
+
+// Añado los event listeners para los botones
+btnSeccion1.addEventListener("click", function() {
+	// Ocultamos todas las secciones excepto la primera
+	seccion1.style.display = "block";
+	seccion2.style.display = "none";
+	seccion3.style.display = "none";
+});
+
+btnSeccion2.addEventListener("click", function() {
+	// Ocultamos todas las secciones excepto la segunda
+	seccion1.style.display = "none";
+	seccion2.style.display = "block";
+	seccion3.style.display = "none";
+});
+
+btnSeccion3.addEventListener("click", function() {
+	// Ocultamos todas las secciones excepto la segunda
+	seccion1.style.display = "none";
+	seccion2.style.display = "none";
+	seccion3.style.display = "block";
+});
+
+//-------------------Clases---------------------------
 class Punto {
     constructor(x, y, z, d) {
         this.x = x;
@@ -19,7 +52,7 @@ class Linea {
         let yf = this.pfinal.y;
         let xi = this.pinicial.x;
         let yi = this.pinicial.y;
-        return Math.sqrt(Math.pow(xf-xi , 2)+Math.pow(yf - yi , 2)).toFixed(3);
+        return parseFloat(Math.sqrt(Math.pow(xf-xi , 2)+Math.pow(yf - yi , 2)).toFixed(3));
     }
 
     getpendiente(input_pendiente){
@@ -56,138 +89,86 @@ class Linea {
 
 class Conjunto {
     constructor(){
-        this.array_puntos = [];
-        this.array_lineas = [];
-    }
-    inputdatoshard(){
-        //input datos de prueba hardcodeados
-            this.array_puntos.push(
-            new Punto (0,0,4.25, "punto1"),
-            new Punto (0,0,4.15, "punto2"),
-            new Punto (2,2,5.5, "punto3"),
-            new Punto (2,2,5.5, "punto4"),
-            new Punto (2,-2,0, "punto5"),
-            new Punto (-2,-2,0, "punto6"),
-        )
-        for (let i = 0; i < this.array_puntos.length-1; i++) {
-            let linea = new Linea (this.array_puntos[i + 1] , this.array_puntos[i]);
-            this.array_lineas.push(linea);
-        }  
+        this.arrayPuntos = [];
+        this.arrayLineas = [];
     }
 
-    new_point(x,y,z,d) {
+    //guardo en mis arrays de puntos y lineas los datos correspondientes
+    nuevoDato(x,y,z,d) { 
         if (!isNaN(x)) {
             if (!isNaN(y)) {
                 if (!isNaN(z)) {
-                    let point = new Punto (x, y, z, d );
-                    this.array_puntos.push (point);
-                    if (this.array_puntos.length > 1) {
-                        let linea = new Linea (this.array_puntos[this.array_puntos.length - 2] , this.array_puntos[this.array_puntos.length - 1]);
-                        this.array_lineas.push(linea);
+                    let point = new Punto (parseFloat(x), parseFloat(y), parseFloat(z), d);
+                    this.arrayPuntos.push (point);
+                    if (this.arrayPuntos.length > 1) {
+                        let linea = new Linea (this.arrayPuntos[this.arrayPuntos.length - 2] , this.arrayPuntos[this.arrayPuntos.length - 1]);
+                        this.arrayLineas.push(linea);
                     }
-            
                 }    
             }   
         }
     }
 
-
     lineaDistancia() {
-        return this.array_lineas.map((element) => element.getDist());
+        return this.arrayLineas.map((element) => element.getDist());
     }
     lineaPendiente() {
-        let select_html = parseInt(document.getElementById("slc_Pendiente").value);
+        let select_html = parseInt(document.getElementById("slc_Pendiente").value); //selecciono el formato (%, y/x, grados)
 
-        return this.array_lineas.map((element) => element.getpendiente(select_html));
+        return this.arrayLineas.map((element) => element.getpendiente(select_html));
     }
 
-    consola() {
-        console.log(this.array_puntos);
-        console.log(this.array_lineas);
-        // console.table(this.lineaDistancia());
-        // console.table(this.lineaPendiente());
+    consoleLog() {
+        console.log(this.arrayPuntos);
+        console.log(this.arrayLineas);
+        console.table(this.lineaDistancia());
+        console.table(this.lineaPendiente());
     }
     
-    //imprimir en tablas HTML
-    lineas_to_table(){
-        
-        let tabla_pi_html = document.getElementById("table_pi");
-        let tabla_pf_html = document.getElementById("table_pf");
-        tabla_pf_html.innerHTML = ""; //limpio el cuerpo de la tabla
-        tabla_pi_html.innerHTML = "";
-
-        // Crear una nueva fila en las 2 tablas
-        for (const lineas of this.array_lineas) {
-            let fila_pf = tabla_pf_html.insertRow();
-            let celda1_pf = fila_pf.insertCell();
-            let celda2_pf = fila_pf.insertCell();
-            let celda3_pf = fila_pf.insertCell();
-            let celda4_pf = fila_pf.insertCell();
-            celda1_pf.innerHTML = lineas.pfinal.x;
-            celda2_pf.innerHTML = lineas.pfinal.y;
-            celda3_pf.innerHTML = lineas.pfinal.z;
-            celda4_pf.innerHTML = lineas.pfinal.d;
-
-            let fila_pi = tabla_pi_html.insertRow();
-            let celda1_pi = fila_pi.insertCell();
-            let celda2_pi = fila_pi.insertCell();
-            let celda3_pi = fila_pi.insertCell();
-            let celda4_pi = fila_pi.insertCell();
-            celda1_pi.innerHTML = lineas.pinicial.x;
-            celda2_pi.innerHTML = lineas.pinicial.y;
-            celda3_pi.innerHTML = lineas.pinicial.z;
-            celda4_pi.innerHTML = lineas.pinicial.d;
+    getPuntos(){
+        return this.arrayPuntos;
+    }
+    getLineas(){
+        return this.arrayLineas;
+    }
+    getArrayX(){
+        return this.arrayPuntos.map(punto => punto.x);
+    }
+    getArrayY(){
+        return this.arrayPuntos.map(punto => punto.y);
+    }
+    getArrayZ(){
+        return this.arrayPuntos.map(punto => punto.z);
+    }
+    getArrayD(){
+        return this.arrayPuntos.map(punto => punto.d);
+    }
+    getArrayXPerfil(){//obtengo un array con las distancias sumadas para cargar en mi perfil longitudinal
+        let x = [0];
+        for (let i = 0; i < this.lineaDistancia().length; i++) {
+            x[i+1] = x[i]+this.lineaDistancia()[i];
         }
-    }    
-    prop_to_table(){
-        document.getElementById("t_prop").innerHTML = `
-            <tr >
-                <th colspan="2">Propiedades</th>
-                </tr>
-                <tr>
-                <th>Distancia</th>
-                <th>Pendiente</th>
-                </tr>    
-            <tbody id="tbody_prop">
-            </tbody>
-        `; //sobrescribo la tabla
-
-        const tbodyProp_html = document.getElementById("tbody_prop");
-
-        // Crear una nueva fila y agrega las propiedades
-        for (let i = 0; i < this.array_lineas.length; i++) {
-            let fila = tbodyProp_html.insertRow();
-            let cell1 = fila.insertCell();
-            let cell2 = fila.insertCell();
-            cell1.innerHTML = this.lineaDistancia()[i] + " m";
-            cell2.innerHTML = this.lineaPendiente()[i];
-        }
+        return x;
+    }
+    getCantLineas(){
+        return this.arrayLineas.length;
+    }
+    getCantPuntos(){
+        return this.arrayPuntos.length;
+    }
+    limpiarDatos(){
+        this.arrayPuntos = [];
+        this.arrayLineas = [];
     }
 
-    //Local Storage
-    guardarlocal() {
-        if (this.array_lineas.length > 1) {
-            localStorage.removeItem("puntos");
-            localStorage.setItem("puntos", JSON.stringify(this.array_puntos));
+    cargarDatos(array_puntos_in){
+        for (const punto of array_puntos_in) {
+            this.nuevoDato(punto.x, punto.y, punto.z, punto.d );
         }
-    }
-    restaurarlocal() {
-        let array_puntos_stg = JSON.parse(localStorage.getItem("puntos"));
-        this.array_puntos = [];
-        this.array_lineas = [];
-        for (const punto of array_puntos_stg) {
-            let point = new Punto( punto.x, punto.y, punto.z, punto.d );
-            this.array_puntos.push(point);
-        }
-        for (let i = 0; i < this.array_puntos.length-1; i++) {
-            let linea = new Linea (this.array_puntos[i] , this.array_puntos[i + 1]);
-            this.array_lineas.push(linea);
-        }  
     }
 }
 
-const conjpuntos = new Conjunto ();
-
+//--------------Botones-----------------------------------------------------------------------------------------------------------------------------------------
 // accion del boton de cargar datos
 document.getElementById('btnpunto').addEventListener( 'click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
@@ -197,46 +178,138 @@ document.getElementById('btnpunto').addEventListener( 'click', (event) => {
     const descripcion = document.getElementById('descripcion').value;
 
     console.clear();
-    conjpuntos.new_point (x, y, z, descripcion);
-    conjpuntos.lineas_to_table();
-
-    conjpuntos.consola();
+    conjpuntos.nuevoDato (x, y, z, descripcion);
+    lineasATabla();
+    propATabla();
 });
 
-// accion del boton de guardar los datos en el local storage datos
+// accion del boton de guardar los datos en el local storage
 document.getElementById('btnguardar').addEventListener( 'click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
-    conjpuntos.guardarlocal();
-
+    guardarLocal();
 });
-
 
 // accion del boton de Resutarar los datos desde el local storage
 document.getElementById('btnrestaurar').addEventListener( 'click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
-    conjpuntos.restaurarlocal();
-    conjpuntos.lineas_to_table();
-    // console.clear();
-
-    // conjpuntos.consola();
+    restaurarLocal();
+    lineasATabla();
 });
+
 // accion del boton Mostrar propiedad de Distancia y angulo entre lineas
 document.getElementById('btnpropiedades').addEventListener( 'click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
-    conjpuntos.prop_to_table();
-    // console.clear();
-    // conjpuntos.consola();
-
+    propATabla();
 });
 
 
 // accion del boton Mostrar propiedad de Distancia y angulo entre lineas
 document.getElementById('slc_Pendiente').addEventListener( 'change', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
-    conjpuntos.prop_to_table();
+    propATabla();
     // console.clear();
     // conjpuntos.consola();
     
 });
+// accion del boton Mostrar propiedad de Distancia y angulo entre lineas
+document.getElementById('Json').addEventListener( 'change', (event) => {
+    event.preventDefault(); // Evita que el formulario se envíe de manera predeterminada
+    CargarJson();
+});
 
 
+
+
+//--------------------funciones-------------------------------------------------------------------------------------
+
+//cargo del archivo datos.json los puntos en forma asincronica
+function CargarJson(){
+    fetch("./datos.json")
+    .then((res) => res.json())
+    .then((data) => {
+        conjpuntos.limpiarDatos();
+        conjpuntos.cargarDatos(data);
+        lineasATabla();
+        propATabla();
+    })
+}
+//imprimir en tablas HTML las lineas
+function lineasATabla(){ 
+    
+    let tabla_pi_html = document.getElementById("table_pi");
+    let tabla_pf_html = document.getElementById("table_pf");
+    tabla_pf_html.innerHTML = ""; //limpio el cuerpo de la tabla
+    tabla_pi_html.innerHTML = "";
+
+    // Crear una nueva fila en las 2 tablas
+    for (const lineas of conjpuntos.getLineas()) {
+        let fila_pf = tabla_pf_html.insertRow();
+        let celda1_pf = fila_pf.insertCell();
+        let celda2_pf = fila_pf.insertCell();
+        let celda3_pf = fila_pf.insertCell();
+        let celda4_pf = fila_pf.insertCell();
+        celda1_pf.innerHTML = lineas.pfinal.x;
+        celda2_pf.innerHTML = lineas.pfinal.y;
+        celda3_pf.innerHTML = lineas.pfinal.z;
+        celda4_pf.innerHTML = lineas.pfinal.d;
+
+        let fila_pi = tabla_pi_html.insertRow();
+        let celda1_pi = fila_pi.insertCell();
+        let celda2_pi = fila_pi.insertCell();
+        let celda3_pi = fila_pi.insertCell();
+        let celda4_pi = fila_pi.insertCell();
+        celda1_pi.innerHTML = lineas.pinicial.x;
+        celda2_pi.innerHTML = lineas.pinicial.y;
+        celda3_pi.innerHTML = lineas.pinicial.z;
+        celda4_pi.innerHTML = lineas.pinicial.d;
+    }
+}    
+//imprime en tabla las propiedades de las lineas
+function propATabla(){
+    document.getElementById("t_prop").innerHTML = `
+        <tr >
+            <th colspan="2">Propiedades</th>
+            </tr>
+            <tr>
+            <th>Distancia</th>
+            <th id="pendiente">Pendiente</th>
+            </tr>    
+        <tbody id="tbody_prop">
+        </tbody>
+    `; //sobrescribo la tabla
+
+    const tbodyProp_html = document.getElementById("tbody_prop");
+
+    // Crear una nueva fila y agrega las propiedades
+    for (let i = 0; i < conjpuntos.getCantLineas(); i++) {
+        let fila = tbodyProp_html.insertRow();
+        let cell1 = fila.insertCell();
+        let cell2 = fila.insertCell();
+        cell1.innerHTML = conjpuntos.lineaDistancia()[i] + " m";
+        cell2.innerHTML = conjpuntos.lineaPendiente()[i];
+    }
+}
+
+//Guardo los datos en mis arrays de puntos en el Local Storage
+function guardarLocal() {
+        if (conjpuntos.getCantLineas() > 1) {
+            localStorage.removeItem("puntos");
+            localStorage.setItem("puntos", JSON.stringify(conjpuntos.getPuntos()));
+        }
+    }
+
+//restaulo los datos almacenados en el local Storage
+function  restaurarLocal() { 
+        if (localStorage.getItem("puntos") !== null) {
+            conjpuntos.limpiarDatos();
+            conjpuntos.cargarDatos(JSON.parse(localStorage.getItem("puntos")));
+        }
+
+    }
+
+
+
+//--------------------------------------------------------------------------------------------
+//------------------comandos utilizados-------------------
+
+const conjpuntos = new Conjunto ();
